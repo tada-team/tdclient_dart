638cc860309c0b2d663073c4341ea60afff2f5fe
eboshare <t.lila@list.ru>
2020-12-28 02:23:28 +0300

    feat(websocket+http): add exports of dtos

--- lib/widget.dart
+++ lib/widget.dart
@@ -1,7 +1,13 @@
 library widget;
 
+// Core
+export './src/core/core_dtos/core_dtos.dart';
+
 // HTTP
 export './src/http/widget/widget.dart';
+export './src/http/http_dtos/http_dtos.dart';
 
 // WebSocket
 export './src/websocket/widget/widget.dart';
+export './src/websocket/websocket_dtos/websocket_dtos.dart';
+export './src/websocket/websocket_event_names.dart';
