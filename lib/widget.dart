281b4c6d940dc9d17dfa773fc83d3ee9716dbb20
eboshare <t.lila@list.ru>
2020-12-28 01:14:01 +0300

    feat(export): add export of all websocket and http clients

--- lib/widget.dart
+++ lib/widget.dart
@@ -1,1 +1,13 @@
 library widget_api;
+
+// HTTP
+export './src/http/widget/i_widget_http_client.dart';
+export './src/http/widget/widget_http_client.dart';
+export './src/http/widget/cookie_widget_http_client.dart';
+export './src/http/widget/token_widget_http_client.dart';
+
+// WebSocket
+export './src/websocket/widget/i_widget_websocket_client.dart';
+export './src/websocket/widget/widget_websocket_client.dart';
+export './src/websocket/widget/cookie_widget_websocket_client.dart';
+export './src/websocket/widget/token_widget_websocket_client.dart';
